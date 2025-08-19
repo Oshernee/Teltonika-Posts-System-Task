1d7622231832ea05017475c1082110fcb9e1f640
ignpau <ignas.paulauskas@teltonika.lt>
2025-08-19 16:29:22 +0300

    Improved notification logic

--- src/store/Notification.ts
+++ src/store/Notification.ts
@@ -1,21 +1,52 @@
 import { defineStore } from 'pinia'
-import { ref, computed } from 'vue'
-import type Notification from '@/interfaces/Notification'
+import { ref } from 'vue'
+import type Notification from '@/types/Notification'
 
 export const useNotificationStore = defineStore('notification', () => {
   const notifications = ref<Notification[]>([])
+  let globalTimer: number | null = null
 
   const addNotification = (notification: Notification) => {
-    const id = crypto.randomUUID()
-    notifications.value.unshift({ ...notification, id, createdAt: new Date(), duration: 5000 })
+    if (checkForDuplicate(notification.message)) {
+      return
+    }
 
-    setTimeout(() => {
-      removeNotificationById(id)
+    const id = crypto.randomUUID()
+    notifications.value.unshift({ ...notification, id })
+
+    if (!globalTimer) {
+      startGlobalTimer()
+    }
+  }
+
+  const startGlobalTimer = () => {
+    globalTimer = setInterval(() => {
+      if (notifications.value.length > 0) {
+        notifications.value.pop()
+      }
+
+      if (notifications.value.length === 0) {
+        stopGlobalTimer()
+      }
     }, 5000)
+  }
+
+  const stopGlobalTimer = () => {
+    if (globalTimer) {
+      clearInterval(globalTimer)
+      globalTimer = null
+    }
   }
 
   const removeNotificationById = (id: string) => {
     notifications.value = notifications.value.filter((n) => n.id !== id)
+    if (notifications.value.length === 0) {
+      stopGlobalTimer()
+    }
+  }
+
+  const checkForDuplicate = (message: string) => {
+    return notifications.value.some((n) => n.message === message)
   }
 
   return {
--- src/types/Notification.ts
+++ src/types/Notification.ts
@@ -2,6 +2,4 @@ export default interface Notification {
   id?: string
   message: string
   type: 'success' | 'error' | 'info' | 'warning'
-  createdAt?: Date
-  duration?: number
 }
